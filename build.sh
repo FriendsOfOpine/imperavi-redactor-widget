f917c199ec5c3cb20344ad48606e979afadda077
Ryan Mahoney <ryan@virtuecenter.com>
2014-11-27 15:21:59 -0500

    concatenate files

---
+++ build.sh
@@ -0,0 +1,7 @@
+cat assets/plugins/fontcolor/fontcolor.js >> dist/redactor.js
+cat assets/plugins/fontfamily/fontfamily.js >> dist/redactor.js
+cat assets/plugins/table/table.js >> dist/redactor.js
+cat assets/plugins/fullscreen/fullscreen.js >> dist/redactor.js
+cat assets/plugins/fontsize/fontsize.js >> dist/redactor.js
+cat assets/plugins/video/video.js >> dist/redactor.js
+cat assets/redactor.js >> dist/redactor.js
